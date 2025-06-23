1adede573157e02706c63c499ca09987be7b9714
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-06-23 15:53:34 +0200

    feat: add TabBar to seletec configuration tab

--- src/pages/AppsConfiguration.tsx
+++ src/pages/AppsConfiguration.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Tabs from '../components/tabs/Tabs'
+import { useUrlParams } from 'dhis2-semis-functions'
+
+const tabs = [
+  {
+    id: 'student',
+    label: 'Student',
+    component: () => <div>Default Tab Content</div>,
+  },
+  {
+    id: 'staff',
+    label: 'Staff',
+    component: () => <div>Settings Tab Content</div>,
+  }
+]
 
 const AppsConfiguration = () => {
+  const { useQuery } = useUrlParams()
+  const sectionType = useQuery().get('sectionType')
+  const activeTab = tabs.find(tab => tab.id === sectionType) || tabs[0]
+
+
   return (
-    <div>AppsConfiguration</div>
+    <div>
+      <Tabs tabs={tabs} />
+      <div style={{ marginTop: '1rem' }}>
+        {activeTab.component()}
+      </div>
+    </div>
   )
 }
 
