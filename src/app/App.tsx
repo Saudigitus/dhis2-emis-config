0fda1761a91ae7e1b85ec1a3e35a209a884f3114
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-10-13 09:40:58 +0200

    feat: update version and comment out unused imports in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,26 +2,25 @@ import React from 'react'
 import "./App.module.css"
 import "../assets/style/globalStyle.css"
 import { Router } from "../components/routes"
-import { useConfig } from '@dhis2/app-runtime'
+// import { useConfig } from '@dhis2/app-runtime'
 import CustomAppWrapper from './wrapper/AppWrapper';
-import { AppWrapper } from 'dhis2-semis-components'
-import { HashRouter } from 'react-router-dom'
+// import { AppWrapper } from 'dhis2-semis-components'
+// import { HashRouter } from 'react-router-dom'
 
 function ConfigirationsPage() {
-    const { baseUrl } = useConfig()
+    // const { baseUrl } = useConfig()
 
     return (
-
         // <AppWrapper
         //     baseUrl={baseUrl}
         //     dataStoreKey="dataStore/semis/values"
         //     schoolCalendarKey='dataStore/semis/schoolCalendar'
         // >
         //     <HashRouter>
-        //         <CustomAppWrapper>
+                <CustomAppWrapper>
                     <Router />
-        //         </CustomAppWrapper>
-        //    </HashRouter >
+                </CustomAppWrapper>
+        //     </HashRouter >
         // </AppWrapper>
     )
 }
